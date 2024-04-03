daf8812c2cddd7f9c1ff703e7570a5629e970df6
Anjalivlog <139326744+Anjalivlog@users.noreply.github.com>
2024-04-03 12:44:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 function getFormvalue() {
     //Write your code here
+	const firstName = document.forms["form1"]["fname"].value;
+    const lastName = document.forms["form1"]["lname"].value;
+
+    const fullName = firstName + ' ' + lastName;
+
+    alert(fullName);
 
 }
